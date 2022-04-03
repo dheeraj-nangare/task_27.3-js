6bb34bb0ce8e65c7f82dc8d919b25d2d824ddac4
dheeraj-nangare <dheerajnangare293@gmail.com>
2022-04-03 13:52:59 +0530

    first commit

---
+++ problem 4.js
@@ -0,0 +1,32 @@
+//switch method
+
+let weekday=Number(prompt("enter the weekday"))
+switch(weekday)
+{
+    case 0 :
+    console.log("monday");
+     break
+
+    case 1 :
+        console.log("Tuesday");
+         break
+
+        case 2 :
+            console.log("Wednesday");
+            break
+
+            case 3 :
+                console.log("thuresday")
+                break
+
+                case 4 :
+                    console.log("friday")
+                    break
+
+                    case 5 :
+                        console.log("saturday")
+                        break
+                        
+                        default:
+                            conaole.log("enter value upto 6")
+}
--- problem3.js
+++ problem3.js
@@ -1,6 +1,7 @@
-let a = 2
+console.log( 4 < 4)
+let  a = 1
 let b = 2
 let result= a + b < 4 ? "below" : "over"
 console.log(result) 
 
-// over
+// ans is over
